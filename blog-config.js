829926bc46d08299010b475b8450cb82a8aa7bbd
mitoconcrete <xogns98@gmail.com>
2022-08-24 18:09:16 +0900

    feat : 프로필 사진이 올바르게 표기되도록 합니다.

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "mitoconcrete",
   description: "안녕하세요. 3년차 프론트엔드 개발자 mitoconcrete 입니다.",
   author: "mitoconcrete",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://mitoconcrete.github.io",
   links: {
     github: "https://github.com/mitoconcrete",
     email: "mailto:xogns98@gmail.com",
