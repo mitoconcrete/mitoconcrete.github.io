73f024a2659381b75f0e9734f5a7279b350276c2
mitoconcrete <xogns98@gmail.com>
2022-08-24 17:23:36 +0900

    feat : 정보 입력 및 favicon 교체.

--- blog-config.js
+++ blog-config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  title: "hoodie",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
+  title: "mitoconcrete",
+  description: "안녕하세요. 3년차 프론트엔드 개발자 mitoconcrete 입니다.",
+  author: "mitoconcrete",
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
-    github: "https://github.com/devHudi",
-    linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com",
-    instagram: "https://www.instagram.com",
-    email: "mailto:devhudi@gmail.com",
+    github: "https://github.com/mitoconcrete",
+    email: "mailto:xogns98@gmail.com",
   },
   utterances: {
-    repo: "devHudi/gatsby-starter-hoodie",
+    repo: "mitoconcrete/mitoconcrete.github.io",
     type: "pathname",
   },
 }
