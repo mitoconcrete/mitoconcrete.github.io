4c4defb403d4e4525f790ad7b6e6fcddd05510f7
mitoconcrete <xogns98@gmail.com>
2022-10-31 23:24:58 +0900

    add : channel talk 위젯 추가.

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -112,6 +112,54 @@ const Header = ({ toggleTheme }) => {
   const theme = useTheme()
   const [scrollY, setScrollY] = useState()
   const [hidden, setHidden] = useState(false)
+  const ChannelTalkWidjet = () => {
+    return (
+      <script>
+        {(function () {
+          var w = window
+          if (w.ChannelIO) {
+            return (
+              window.console.error ||
+              window.console.log ||
+              function () {}
+            )("ChannelIO script included twice.")
+          }
+          var ch = function () {
+            ch.c(arguments)
+          }
+          ch.q = []
+          ch.c = function (args) {
+            ch.q.push(args)
+          }
+          w.ChannelIO = ch
+          function l() {
+            if (w.ChannelIOInitialized) {
+              return
+            }
+            w.ChannelIOInitialized = true
+            var s = document.createElement("script")
+            s.type = "text/javascript"
+            s.async = true
+            s.src = "https://cdn.channel.io/plugin/ch-plugin-web.js"
+            s.charset = "UTF-8"
+            var x = document.getElementsByTagName("script")[0]
+            x.parentNode.insertBefore(s, x)
+          }
+          if (document.readyState === "complete") {
+            l()
+          } else if (window.attachEvent) {
+            window.attachEvent("onload", l)
+          } else {
+            window.addEventListener("DOMContentLoaded", l, false)
+            window.addEventListener("load", l, false)
+          }
+        })()}
+        {ChannelIO("boot", {
+          pluginKey: "ae9ab0cc-d51c-4fd4-988d-12110708d710",
+        })}
+      </script>
+    )
+  }
 
   const detectScrollDirection = () => {
     if (scrollY >= window.scrollY) {
@@ -139,6 +187,7 @@ const Header = ({ toggleTheme }) => {
 
   return (
     <HeaderWrapper isHidden={hidden}>
+      <ChannelTalkWidjet />
       <Inner>
         <BlogTitle>
           <Link to="/">{title}</Link>
